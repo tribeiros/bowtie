c1328678837ab9a232b1831955e8198c681c48d1
Tiago Ribeiro <tribeiros@indracompany.com>
2018-06-19 17:09:00 -0300

    jt theme

---
+++ jt.bowtie/js/script.js
@@ -0,0 +1,32 @@
+var oldPlayState = 0;
+function $(id) { return document.getElementById(id); }
+
+function trackUpdate(track) 
+{
+	
+	$('title').innerHTML = track.property('title');
+	$('artist').innerHTML = track.property('artist');
+	
+	var totalLength = $('title').innerHTML.length + $('artist').innerHTML.length
+	
+	if(totalLength >= 90)
+	{
+		$('title').style.maxWidth = "230px";
+		$('artist').style.maxWidth = "230px";		
+	}
+	else
+	{
+		$('title').style.maxWidth = "none";
+		$('artist').style.maxWidth = "none";
+	}
+}
+
+/*function artworkUpdate(artURL)
+{
+	if (artURL == "")
+		// use "no art" image
+		$('holder').src = "img/noartwork.png";
+	else
+		// use artURL
+		$('holder').src = artURL;
+}*/
